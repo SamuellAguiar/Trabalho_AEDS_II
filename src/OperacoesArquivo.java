ac199cb90d99770c0e8567863e547efb9dfefd53
SamuellNote <samuellcarlosaguiar@gmail.com>
2025-07-02 16:50:00 -0300

    feat: update OperacoesArquivo.java with new file operation logic

--- src/OperacoesArquivo.java
+++ src/OperacoesArquivo.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class OperacoesArquivo {
